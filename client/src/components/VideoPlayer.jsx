a9aaef1c40a715394a1ae72a3e57eb086a508799
DESKTOP-5R085U6\Administrator <water77lover@gmail.com>
2023-12-20 08:36:31 -0300

    FIx

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,53 +1,90 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 // import Player from '@vimeo/player';
-import accessLocal from '../utils/accessLocal';
-import Player from 'react-player';
+import accessLocal from "../utils/accessLocal";
+import Player from "react-player";
 
 const VideoPlayer = () => {
-
-  // const playerRef = useRef();
   const videoId = accessLocal.loadData("list");
-
+  const playerRef = useRef(null);
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
 
+  function findClosestCommonMultiple(a, b, target) {
+    const lcm = (a * b) / findGCD(a, b);
+    const closestMultiple = Math.round(target / lcm) * lcm;
+    return closestMultiple;
+  }
+
+  function findGCD(a, b) {
+    if (b === 0) {
+      return a;
+    }
+    return findGCD(b, a % b);
+  }
+
   useEffect(() => {
     const updateDimensions = () => {
-      const maxWidth = Math.floor(Math.min(window.innerWidth, (window.innerHeight) * 1.7777));
-      const maxHeight = Math.floor(Math.min(window.innerHeight, window.innerWidth / 1.7777));
-      setWidth(Math.floor(maxWidth));
-      setHeight(Math.floor(maxHeight));
+      const screenWidth = window.innerWidth;
+      const screenHeight = window.innerHeight;
+      let mHeight, mWidth;
+      if (screenWidth * 0.5625 > screenHeight) {
+        mHeight = findClosestCommonMultiple(16, 9, screenHeight);
+        mWidth = (mHeight * 16) / 9;
+      } else {
+        mWidth = findClosestCommonMultiple(16, 9, screenWidth);
+        mHeight = (mWidth * 9) / 16;
+      }
+
+      setWidth(mWidth);
+      setHeight(mHeight);
     };
+
     updateDimensions();
-    window.addEventListener('resize', updateDimensions);
-    return () => {
-      window.removeEventListener('resize', updateDimensions);
+
+    const handleResize = () => {
+      updateDimensions();
     };
-    // eslint-disable-next-line
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // useEffect(() => {
-  //   const player = new Player(playerRef.current, {
-  //     id: videoId,
-  //     width: width,
-  //     height: height,
-  //     title: false,
-  //     byline: false,
-  //     autoplay: true,
-  //     loop: true,
-  //   });
-
-  //   return () => {
-  //     player.unload();
-  //   };
-  //   // eslint-disable-next-line
-  // }, [width, height]);
+  const handleVideoEnded = () => {
+    playerRef.current.seekTo(0);
+    playerRef.current.pause();
+  };
 
   return (
     <>
-      <div className="flex flex-col justify-center rounded-3xl">
-        <Player url={`https://vimeo.com/${videoId}`}  width={width} height={height} controls></Player>
-        {/* <div ref={playerRef} className='player rounded-3xl'></div> */}
+      <div
+        className="flex flex-col justify-center rounded-3xl"
+        style={{ width: width, height: height }}
+      >
+        <Player
+          ref={playerRef}
+          url={`https://vimeo.com/${videoId}`}
+          width="100%"
+          height="100%"
+          controls
+          config={{
+            file: {
+              attributes: {
+                crossOrigin: "true",
+              },
+            },
+            vimeo: {
+              playerOptions: {
+                quality: "1080p",
+                responsive: true,
+              },
+            },
+          }}
+          onEnded={handleVideoEnded}
+        ></Player>
       </div>
     </>
   );
